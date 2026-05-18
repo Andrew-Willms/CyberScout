318d7f6f393b0daf7189008a08fd71624115867e
Andrew Willms <andrew.e.willms@gmail.com>
2026-05-18 13:59:45 -0400

    create match format to make it easy to determine if a Game is compatible with an event schedule/format

---
+++ CyberScout/Domain/GameSpecification/MatchFormat.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using UtilitiesLibrary.Collections;
+
+namespace Domain.GameSpecification;
+
+
+
+public class MatchFormat {
+
+	public static MatchFormat Standard = new() {
+		Alliances = new List<(string allianceName, uint teamCount)> {("Red Alliance", 3), ("Blue Alliance", 3)}.ToReadOnly()
+	};
+
+	public required ReadOnlyList<(string allianceName, uint teamCount)> Alliances { get; init; }
+
+	// TODO make this class equatable
+}
