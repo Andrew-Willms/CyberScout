4d9002194d13a4ac435a0161dfc1c735c7312156
Andrew Willms <andrew.e.willms@gmail.com>
2026-05-21 12:22:33 -0400

    remove unecessary platform specific calls

--- CyberScout/ScoutingApp/AppManagement/ServiceHelper.cs
+++ CyberScout/ScoutingApp/AppManagement/ServiceHelper.cs
@@ -10,15 +10,6 @@ public static class ServiceHelper {
 
 	public static T GetService<T>() => Current.GetService<T>() ?? throw new($"Could not resolve service {typeof(T).Name}");
 
-	private static IServiceProvider Current =>
-#if WINDOWS10_0_17763_0_OR_GREATER
-		MauiWinUIApplication.Current.Services;
-#elif ANDROID
-		IPlatformApplication.Current.Services;
-#elif IOS || MACCATALYST
-		MauiUIApplicationDelegate.Current.Services; // todo figure this out
-#else
-		throw new NotSupportedException();
-#endif
+	private static IServiceProvider Current => IPlatformApplication.Current.Services;
 
 }
